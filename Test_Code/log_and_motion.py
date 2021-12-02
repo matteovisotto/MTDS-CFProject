fac7f3f36cc59b433f5decf15a5cc2ae52ab99d0
Matteo Visotto <matteo.visotto@mail.polimi.it>
2021-12-02 18:08:35 +0100

    Fix logging after test

--- Test_Code/log_and_motion.py
+++ Test_Code/log_and_motion.py
@@ -3,28 +3,38 @@ import time
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.positioning.motion_commander import MotionCommander
 from cflib.utils import uri_helper
 
-from Utility.SafeFlyController import SafeFlyController
+from Utility.PositionLogging import PositionLogging
 
-cf_number = '04'
+cf_number = '07'
 
 uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7' + cf_number)
 logging.basicConfig(level=logging.ERROR)
 
 
-def fly_commands(mc):
-    # use the motion commander variable to write your code
-    time.sleep(1)
-    mc.start_forward(velocity=0.4)
-    mc.stop()
+def x_callback(value):
+    pass
 
 
-if __name__ == '__manin__':
+def motion_commander(scf):
+    with MotionCommander(scf) as mc:
+        time.sleep(1)
+        mc.forward(0.5)
+        mc.circle_right(0.5, velocity=0.2, angle_degrees=180)
+        mc.forward(0.5)
+        mc.circle_right(0.5, velocity=0.2, angle_degrees=180)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
     cflib.crtp.init_drivers()
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
         time.sleep(1)
-        flyController = SafeFlyController(scf)
-        flyController.fly_commands = fly_commands
-        flyController.start_flying()
+        log = PositionLogging(scf, use_default=True, debug_mode=False)
+        log.register_callback('stateEstimate.x', x_callback)
+        log.start_logging()
+        motion_commander(scf)
+        log.stop_logging()
 
--- Utility/PositionLogging.py
+++ Utility/PositionLogging.py
@@ -1,32 +1,33 @@
 from cflib.crazyflie.log import LogConfig
 
 
-class MCPositionLogging:
-    def __init__(self, scf):
-        super.__init__()
-        self.position_estimate = [0, 0, 0]
+class PositionLogging:
+    def __init__(self, scf, use_default=True, debug_mode=False):
+        super().__init__()
+        self.position_estimate = {}
+        self.use_default = use_default
+        self.callbacks = {}
         self.scf = scf
+        self.debug_mode = debug_mode
         logconf = LogConfig(name='Position', period_in_ms=10)
-        logconf.add_variable('stateEstimate.x', 'float')
-        logconf.add_variable('stateEstimate.y', 'float')
-        logconf.add_variable('stateEstimate.z', 'float')
+        if use_default:
+            logconf.add_variable('stateEstimate.x', 'float')
+            self.position_estimate['stateEstimate.x'] = 0
+            logconf.add_variable('stateEstimate.y', 'float')
+            self.position_estimate['stateEstimate.y'] = 0
+            logconf.add_variable('stateEstimate.z', 'float')
+            self.position_estimate['stateEstimate.z'] = 0
         scf.cf.log.add_config(logconf)
-        logconf.data_received_cb.add_callback(self.data_callback)
+        logconf.data_received_cb.add_callback(self._data_callback)
         self.logconf = logconf
-        self.on_x_change = self.placeholder
-        self.on_y_change = self.placeholder
-        self.on_z_change = self.placeholder
-        self.on_variables_changes = self.placeholder
-        self.motion_commander = None
 
-    def data_callback(self, timestamp, data, logconf):
-        self.position_estimate[0] = data['stateEstimate.x']
-        self.position_estimate[1] = data['stateEstimate.y']
-        self.position_estimate[2] = data['stateEstimate.z']
-        self.on_x_change(self.position_estimate[0], self.motion_commander)
-        self.on_y_change(self.position_estimate[1], self.motion_commander)
-        self.on_z_change(self.position_estimate[2], self.motion_commander)
-        self.on_variables_changes(self.position_estimate, self.motion_commander)
+    def _data_callback(self, timestamp, data, logconf):
+        if self.debug_mode:
+            print(data)
+        for key in self.position_estimate:
+            self.position_estimate[key] = data[key]
+            if self.callbacks[key] is not None:
+                self.callbacks[key](data[key])
 
     def start_logging(self):
         self.logconf.start()
@@ -34,26 +35,18 @@ class MCPositionLogging:
     def stop_logging(self):
         self.logconf.stop()
 
-    def placeholder(self, value, mc):
-        pass
+    def get_value(self, key):
+        if self.position_estimate[key] is not None:
+            return self.position_estimate[key]
+        else:
+            raise IndexError('Index not found')
 
-    def set_motion_commander(self, mc):
-        self.motion_commander = mc
+    def get_last_values(self):
+        return self.position_estimate
 
-    def set_on_x_changed(self, callback):
-        self.on_x_change = callback
+    def add_log_variable(self, variable, data_type):
+        self.logconf.add_variable(variable, data_type)
+        self.position_estimate[variable] = None
 
-    def set_on_y_changed(self, callback):
-        self.on_y_change = callback
-
-    def set_on_z_changed(self, callback):
-        self.on_z_change = callback
-
-    def get_last_x(self):
-        return self.position_estimate[0]
-
-    def get_last_y(self):
-        return self.position_estimate[1]
-
-    def get_last_z(self):
-        return self.position_estimate[2]
+    def register_callback(self, variable, callback):
+        self.callbacks[variable] = callback
