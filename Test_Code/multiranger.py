7f23e15e6237a0cb1f8691b442077cd453aed2c5
Matteo Visotto <matteo.visotto@mail.polimi.it>
2021-12-10 17:47:57 +0100

    Modify examples

--- Test_Code/multiranger.py
+++ Test_Code/multiranger.py
@@ -20,7 +20,7 @@ logging.basicConfig(level=logging.ERROR)
 
 
 def is_close(range):
-    MIN_DISTANCE = 0.6  # m
+    MIN_DISTANCE = 0.5  # m
 
     if range is None:
         return False
@@ -35,16 +35,17 @@ if __name__ == '__main__':
     cf = Crazyflie(rw_cache='./cache')
     with SyncCrazyflie(URI, cf=cf) as scf:
         log = PositionLogging(scf, debug_mode= True)
+        log.add_log_variable('range.front', 'uint16_t')
         log.start_logging()
         with MotionCommander(scf, default_height=0.2) as motion_commander:
             with Multiranger(scf) as multiranger:
                 keep_flying = True
 
-                velocity_x = 0.2
+                velocity_x = 0.3
                 velocity_y = 0.0
 
                 while keep_flying:
-                    VELOCITY = 0.1
+                    VELOCITY = 0.3
 
                     if log.get_value('stateEstimate.x') < -0.2:
                         keep_flying = False
@@ -67,4 +68,3 @@ if __name__ == '__main__':
 
                     time.sleep(0.1)
             log.stop_logging()
-            print('Demo terminated!')
