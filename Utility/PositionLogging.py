93e54e23e46f64c498a7d013a1a895088e026cb9
Matteo Visotto <matteo.visotto@mail.polimi.it>
2021-12-17 15:36:43 +0100

    Add comment in Logging

--- Utility/PositionLogging.py
+++ Utility/PositionLogging.py
@@ -1,7 +1,14 @@
+"""
+Simplified version of logging library
+"""
+
 from cflib.crazyflie.log import LogConfig
 
 
 class PositionLogging:
+    # Class init
+    # use_default - Add stateEstimate x,y and z variable
+    # debug_mode - If True, print variables in console
     def __init__(self, scf, use_default=True, debug_mode=False):
         super().__init__()
         self.position_estimate = {}
@@ -9,7 +16,7 @@ class PositionLogging:
         self.callbacks = {}
         self.scf = scf
         self.debug_mode = debug_mode
-        logconf = LogConfig(name='Position', period_in_ms=10)
+        logconf = LogConfig(name='LoggingDefault', period_in_ms=10)
         if use_default:
             logconf.add_variable('stateEstimate.x', 'float')
             self.position_estimate['stateEstimate.x'] = 0
@@ -29,24 +36,31 @@ class PositionLogging:
             if key in self.callbacks:
                 self.callbacks[key](data[key])
 
+    # Function that has to be called to start logging
     def start_logging(self):
         self.logconf.start()
 
+    # Function that has to be called before terminate the script
     def stop_logging(self):
         self.logconf.stop()
 
+    # Get a variable value in its last stare by key
     def get_value(self, key):
         if key in self.position_estimate:
             return self.position_estimate[key]
         else:
             raise IndexError('Index not found')
 
+    # Returns all the variables added for logging
     def get_last_values(self):
         return self.position_estimate
 
+    # Add a new variable using name and data type according to crazyflie TOC documentation
     def add_log_variable(self, variable, data_type):
         self.logconf.add_variable(variable, data_type)
         self.position_estimate[variable] = None
 
+    # Register a new callback for a variable already present, use the name specified in the TOC
+    # This function is called every time the value changes
     def register_callback(self, variable, callback):
         self.callbacks[variable] = callback
